bfee3d8b6d93a29be34c9341e8ed907e084bdd55
chrisbennett16 <christopher.bennett@exeter.ox.ac.uk>
2013-11-25 22:56:40 +0000

    added in an interaction term between planets

--- 2planetsellipse.py
+++ 2planetsellipse.py
@@ -8,7 +8,8 @@ Created on Fri Nov 22 20:50:03 2013
 #planet masses dependent on already defined values so can retrieve from that however not representative
 #travelling too fast due to step errors could correct by altering velocity or acceleration
 #for a transit with 0.5 ecc the accel must be multiplied by 1.21459233575 to get the correct period, get this by minimisation
-#will continue anyway to get interacting orbits in the same way assumin no path change
+#will continue anyway to get interacting orbits in the same way assuming no path change
+#add in something to detect if paths cross as it will be unstable
 from pylab import*
 import matplotlib.pylab as plt
 
@@ -18,8 +19,8 @@ G=6.67384e-11
 
 def effaccstar(angle,ecc,a0):
     distance=a0/(1+ecc*cos(angle))
-    x1=distance*cos(angle1)
-    y1=distance*sin(angle1)
+    x1=distance*cos(angle)
+    y1=distance*sin(angle)
     if sin(angle)==0 and x1>0:
         angle02=3*pi/2
     elif sin(angle)==0 and x1<0:
@@ -33,9 +34,39 @@ def effaccstar(angle,ecc,a0):
     angle03=angle02-angle+pi
     #angle4 is the angle between velocity direction and perpendicular to the radius    
     angle04=pi/2-angle03
-    
-    effacc=cos(angle03)/(distance**2)
+    effacc=-cos(angle03)/(distance**2)
     return effacc
+
+def effaccplanet(anglea, ecca, a0, x2, y2):
+    distance=a0/(1+ecca*cos(anglea))
+    x1=distance*cos(anglea)
+    y1=distance*sin(anglea)
+    if sin(anglea)==0 and x1>0:
+        angle02=3*pi/2
+    elif sin(anglea)==0 and x1<0:
+        angle02=pi/2
+    else:
+        angle02=atan(-(cos(anglea)+ecca)/sin(anglea))
+    if anglea>pi:
+        angle02=angle02-pi
+    if angle02<0:
+        angle02=angle02+2*pi
+    #now we have the correct gradient angle need to compare to position gradients
+    if x1==x2 and y2>y1:
+        angle03=pi/2
+    elif x1==x2 and y1>y2:
+        angle03=3*pi/2
+    angle03=atan((y2-y1)/(x2-x1))
+    if y2>y1 and angle03<0:
+        angle03=angle03+pi
+    if y2<y1 and angle03>0:
+        angle03=angle03+pi
+    elif y2<y1 and angle03<0:
+        angle03=angle03+ 2*pi
+    angle04=angle03-angle02    
+    
+    effaccp=-cos(angle04)/((x1-x2)**2+(y1-y2)**2)
+    return effaccp
     
 
 # inouts, require masses periods radii and step number for 2nd planet orbit
@@ -76,10 +107,15 @@ planet_x1=0.0
 planet_y1=latus1
 plot2=[]
 plot1=[]
-print velocity1
-    
 
-for t in range(1,int(2*step_number)):
+transit_on=1
+transit_on2=1
+s=0
+transit_counter1=0
+transit_counter2=0
+print period1, period2
+
+for t in range(1,int(20*step_number)):
     if (planet_x1>0 and planet_y1>=0):    
         angle1=atan(planet_y1/planet_x1)
     elif (planet_x1<0):
@@ -97,9 +133,9 @@ for t in range(1,int(2*step_number)):
     else:
         angle2=atan(planet_y2/planet_x2)+2*pi
     distance1=latus1/(1+ecc1*cos(angle1))
-    distance2=latus2/(1+ecc2*cos(angle2)) 
-    planet_a1=-effaccstar(angle1,ecc1,latus1)*(G*star_mass)
-    planet_a2=-effaccstar(angle2,ecc2,latus2)*(G*star_mass)
+    distance2=latus2/(1+ecc2*cos(angle2))
+    planet_a1=effaccstar(angle1,ecc1,latus1)*(G*star_mass)+effaccplanet(angle1, ecc1, latus1, planet_x2, planet_y2)*G*planet_mass2
+    planet_a2=effaccstar(angle2,ecc2,latus2)*(G*star_mass)+effaccplanet(angle2, ecc2, latus2, planet_x1, planet_y1)*G*planet_mass1
     velocity1=velocity1+planet_a1*period1/step_number
     velocity2=velocity2+planet_a2*period1/step_number
     angle1=angle1+velocity1*period1/(step_number*distance1)
@@ -109,16 +145,29 @@ for t in range(1,int(2*step_number)):
     planet_x1=distance1*cos(angle1)
     planet_y1=distance1*sin(angle1)
     planet_x2=distance2*cos(angle2)
-    planet_y2=distance2*sin(angle2)    
-    velocity3=(G*star_mass*(2/distance1-1/smAxis1))**(0.5)
-    if t==1:
-        print planet_a1*period1/step_number
-        print velocity1
-        print velocity3
-    if t==100000:
-        print planet_x1
-    plot2.append(velocity1)
-    plot1.append(velocity3)
+    planet_y2=distance2*sin(angle2)
+    #transit detector required
+    if planet_x1<star_radius+planet_radius1 and planet_x1>-star_radius-planet_radius1 and planet_y1>0 and transit_on==1:
+        transit_on=0
+        transit_counter1=transit_counter1+1
+        print 'Transit for planet 1 '+ str(t)
+        if s!=0 and transit_counter1!=2:
+            plot1.append(t-s)
+        s=t
+    elif transit_on==0 and not (planet_x1<star_radius+planet_radius1 and planet_x1>-star_radius-planet_radius1 and planet_y1>0):
+        transit_on=1
+        print 'Transit finished ' + str(t-s)
+ 
+    if planet_x2<star_radius+planet_radius2 and planet_x2>-star_radius-planet_radius2 and planet_y2>0 and transit_on2==1:
+        print 'To2 '+ str(t)
+        transit_on2=0
+        transit_counter2=transit_counter2+1
+    elif transit_on2==0 and not (planet_x2<star_radius+planet_radius2 and planet_x2>-star_radius-planet_radius2 and planet_y2>0):
+        transit_on2=1
+        print 'Tf' +str(t-1)    
+    
+    
+    
 plot(plot1)
-plot(plot2)
+
     
